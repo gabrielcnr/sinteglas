01e4c1983b7d36a1ed045ec8df7d5b3261db1002
gabriel.reis <gabrielcnr@gmail.com>
2015-10-23 07:25:43 +0100

    Create main window skeleton

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
 from setuptools import setup, find_packages
 
+def get_version():
+    import sinteglas
+    return sinteglas.version
+
+
+entry_points = {
+    'console_scripts': [
+        'sinteglas = sinteglas.main:main',
+    ],
+}
+
+
 setup(
     name='sinteglas',
-    version='1.0',
+    version=get_version(),
     packages=find_packages(),
+    entry_points=entry_points,
 )
---
+++ sinteglas/main.py
@@ -0,0 +1,20 @@
+import enaml
+from enaml.qt.qt_application import QtApplication
+from sinteglas.controller import SinteglasOrderController
+from sinteglas.model import create_database_session
+
+with enaml.imports():
+    from sinteglas.views.main import Main
+
+
+def main():
+    app = QtApplication()
+    session = create_database_session()
+    controller = SinteglasOrderController(session=session)
+    view = Main(controller=controller)
+    view.show()
+    app.start()
+
+
+if __name__ == '__main__':
+    main()
