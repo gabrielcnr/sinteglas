56eb29eb58827ee643736a903da0ec353d4f2410
gabriel.reis <gabrielcnr@gmail.com>
2016-04-09 21:52:09 +0100

    Observacoes

--- sinteglas/gui/controller.py
+++ sinteglas/gui/controller.py
@@ -2,12 +2,15 @@ from atom.api import Atom, List, Typed, observe
 
 from sinteglas.pedidos import Pedido
 from sinteglas.pedidos._pedido import ItemPedido
+from sinteglas.pedidos.observacao import ObservacaoItemPedido
 
 
 class PedidosController(Atom):
     pedidos = List(Pedido)
     pedido_selecionado = Typed(Pedido)
     itens_de_pedido = List(ItemPedido)
+    item_de_pedido_selecionado = Typed(ItemPedido)
+    observacoes_do_item_de_pedido_selecionado = List(ObservacaoItemPedido)
 
     @observe('pedido_selecionado')
     def atualizar_itens_de_pedido_para_o_pedido_selecionado(self, change=None):
@@ -15,3 +18,15 @@ class PedidosController(Atom):
             self.itens_de_pedido = self.pedido_selecionado.itens
         else:
             self.itens_de_pedido = []
+
+    @observe('itens_de_pedido')
+    def resetar_item_de_pedido_selecionado(self, change=None):
+        self.item_de_pedido_selecionado = None
+
+    @observe('item_de_pedido_selecionado')
+    def atualizar_observacoes_do_item_de_pedido_selecionado(self, change=None):
+        if self.item_de_pedido_selecionado:
+            self.observacoes_do_item_de_pedido_selecionado = self.item_de_pedido_selecionado.observacoes
+        else:
+            self.observacoes_do_item_de_pedido_selecionado = []
+
